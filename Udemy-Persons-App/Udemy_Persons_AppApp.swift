190faa2a01bca72102e4a96a2addc52059795a74
hhakangull <hakanngul@icloud.com>
2023-07-01 19:42:11 +0100

    Initial Commit

---
+++ Udemy-Persons-App/Udemy_Persons_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_Persons_AppApp.swift
+//  Udemy-Persons-App
+//
+//  Created by Hakan Gül on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_Persons_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
